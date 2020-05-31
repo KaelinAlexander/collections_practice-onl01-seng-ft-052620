cfc9f98147add6f3a26332a4e2087f9598fb24e7
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-31 15:59:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,70 @@
+def sort_array_asc(array)
+	array.sort
+end
 
+def sort_array_desc(array)
+	array.sort
+	array.reverse
+end
+
+def sort_array_char_count(array)
+	array.sort{|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+	array.sort do |a, b|
+		if a[x] == 1 && b[x] == 2
+			1
+		else
+			0
+		end 
+	end
+end
+
+def reverse_array(integer)
+	integer.sort
+	integer.reverse
+end
+
+def kesha_maker(array)
+	array.each do |word|
+		keshaed=word[2].sub([2],"$")
+		kesha_array << keshaed
+	end
+	kesha_array
+	puts kesha_array
+end
+
+def find_a(array)
+	array.each do |word|
+		if word.start_with?("a")
+			starts_with_a << word
+		end
+	starts_with_a
+end
+
+def find_a(array)
+	array.select do |word|
+		word[0]=="a"
+	end
+end
+
+def sum_array(y)
+sum=0
+	array.each do |integer|
+		sum+= integer
+	end
+sum
+end
+
+def add_s(array)
+	array.each_with_index.collect{|word, index|}
+		if index != 1
+			word.insert[-1, "s"]
+		with_s << word
+		else
+			with_s << word
+		end
+	end
+	with_s
+end
